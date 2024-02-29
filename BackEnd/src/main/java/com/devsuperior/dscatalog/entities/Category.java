be4590f40027d45669f8d1a6a9ab6ef3e7f696ad
vitorlana45 <vitoresmerio00@gmail.com>
2024-02-29 18:45:37 -0300

    H2 database

--- BackEnd/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ BackEnd/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,14 +1,20 @@
 package com.devsuperior.dscatalog.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
--- BackEnd/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ BackEnd/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,23 +1,27 @@
 package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping
     public ResponseEntity<List<Category>> findAll() {
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L, "books"));
-        list.add(new Category(2L, "Electronics"));
+    List<Category> list = categoryService.findAll();
         return ResponseEntity.ok().body(list);
     }
+
 
 }

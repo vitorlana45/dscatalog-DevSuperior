f03dbe501cc2c26dfea661842f00ead59aa98016
vitorlana45 <vitoresmerio00@gmail.com>
2024-03-02 17:44:05 -0300

    pagination

--- BackEnd/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ BackEnd/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,15 +1,18 @@
 package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
-import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -19,8 +22,13 @@ public class CategoryResource {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        List<CategoryDTO> list = categoryService.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO>list = categoryService.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
@@ -38,13 +46,13 @@ public class CategoryResource {
     }
 
     @PutMapping(value = "{id}")
-    public ResponseEntity<CategoryDTO> update(@PathVariable long id,@RequestBody CategoryDTO entity) {
+    public ResponseEntity<CategoryDTO> update(@PathVariable long id, @RequestBody CategoryDTO entity) {
         categoryService.update(id, entity);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping(value = "{id}")
-    public ResponseEntity<CategoryDTO> delete (@PathVariable long id){
+    public ResponseEntity<CategoryDTO> delete(@PathVariable long id) {
         categoryService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
--- BackEnd/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ BackEnd/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -8,11 +8,12 @@ import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,9 +23,9 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<CategoryDTO> findAll() {
-        List<Category> list = repository.findAll();
-        return list.stream().map(x -> new CategoryDTO(x)).toList();
+    public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+        Page<Category> listPage = repository.findAll(pageRequest);
+        return listPage.map(x -> new CategoryDTO(x));
     }
 
     @Transactional(readOnly = true)
